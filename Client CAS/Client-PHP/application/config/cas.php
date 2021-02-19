7241eb34e4208d3415cdf3d282217fce4e6725e6
GeGlory <irvansuryadi3@gmail.com>
2021-02-19 16:31:44 +0700

    TA irvan

---
+++ Client CAS/Client-PHP/application/config/cas.php
@@ -0,0 +1,6 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$config['cas_server_url'] = 'https://cas.example.org/cas';
+$config['phpcas_path'] = '/usr/share/php/phpCAS-master';
+$config['cas_debug']   = TRUE;
+$config['cas_disable_server_validation'] = TRUE;
